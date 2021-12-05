6c466b5d8159d05560719b8a47820eed4ef3dab9
Jaruwat Pohong <jara7897@gmail.com>
2021-12-05 19:49:26 +0700

    feat: implement dialog

--- app/src/main/java/com/example/todosync/MainActivity.java
+++ app/src/main/java/com/example/todosync/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.todosync;
 
+import android.app.Dialog;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -21,7 +22,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.todosync.databinding.ActivityMainBinding;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         ImageButton btnUser = findViewById(R.id.btnUser);
+        //mBottomSheetDialog.getWindow().setWindowAnimations(R.style.MaterialDialogSheet);
 
         btnUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +60,24 @@ public class MainActivity extends AppCompatActivity {
     public void openDrawer() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.openDrawer(GravityCompat.START);
+    }
+
+    public Dialog mBottomSheetDialog;
+    public void showSlideUp(View btn) {
+        mBottomSheetDialog = new Dialog(this, R.style.MaterialDialogSheet);
+        mBottomSheetDialog.setContentView(R.layout.dialog_add_todo); // your custom view.
+        //mBottomSheetDialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        mBottomSheetDialog.getWindow().setGravity(Gravity.BOTTOM);
+        mBottomSheetDialog.setCancelable(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mBottomSheetDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            mBottomSheetDialog.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        mBottomSheetDialog.show();
+    }
+
+    public void hideSlideUp(View btn) {
+        mBottomSheetDialog.hide();
     }
 
 }
