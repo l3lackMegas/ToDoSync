b8bf6c0c1988ba617489b78cfb3d64866560d1a1
M1zTerP <sawagan@hotmail.com>
2021-12-08 17:43:48 +0700

    capture feature

--- app/src/main/java/com/example/todosync/ManageUserActivity.java
+++ app/src/main/java/com/example/todosync/ManageUserActivity.java
@@ -2,8 +2,11 @@ package com.example.todosync;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ManageUserActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class ManageUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_user);
+
+        TextView changePic = findViewById(R.id.changePicBtn);
+        changePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent captureScreen = new Intent(ManageUserActivity.this, capture.class);
+                startActivity(captureScreen);
+            }
+        });
     }
 
     public void closeManageAccount(View v) {
